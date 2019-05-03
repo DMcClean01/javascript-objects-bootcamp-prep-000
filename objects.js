50f6f6e42d6e107b3094a0685e24c02a9b76af21
Daniel McClean <dmcclean01@hotmail.com>
2019-05-03 15:44:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle;
+  playlist[songTitle] = 'All About Me';
 }
 
 function removeFromPlaylist(playlist, artistName) {
